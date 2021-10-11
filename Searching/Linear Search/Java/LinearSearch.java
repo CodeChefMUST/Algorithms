4fb0630dd5c5cc8e85c7e946586ede12993b9551
naresh1205 <f20190638@goa.bits-pilani.ac.in>
2021-10-11 21:43:22 +0530

    Create LinearSearch.java

---
+++ Searching/Linear Search/Java/LinearSearch.java
@@ -0,0 +1,21 @@
+public class LinearSearch
+{
+	/*
+		Linear search function:
+
+		-Takes in two arguments 
+			1. int array
+			2. int target 
+		
+		-The function returns the index of a target element if found else returns -1
+	*/
+	public static int linear_search(int[] arr, int target)
+	{
+		for(int i = 0; i < arr.length; i++)
+		{
+			if(arr[i] == target)
+				return i;
+		}
+		return -1;
+	}
+}
