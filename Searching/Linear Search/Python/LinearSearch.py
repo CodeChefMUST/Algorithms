631df6ea2ff5a04b24c6c65d14f152f40a2a3f5f
ritzz16 <91181981+ritzz16@users.noreply.github.com>
2021-10-13 11:35:26 +0530

    Update LinearSearch.py

--- Searching/Linear Search/Python/LinearSearch.py
+++ Searching/Linear Search/Python/LinearSearch.py
@@ -1,27 +1,33 @@
-def LinearSearch(arr , key):
+def LinearSearch(array , key):
 
-    for i in range(0, len(arr)):
+    for i in range(0, len(array)):
       
-        if (arr[i] == key):
+        if (array[i] == key):
           
             print("Key present at index:",i)
             return 0;
           
     print("Key not present!");
         
-      
-    
+        
 # Sample Input 1 :
-# arr = [21 , 90 , -32 , 78 , 52]
+# array = [21 , 90 , -32 , 78 , 52]
 # key = 78
 
 # Output 1 :
 # Key present at index: 3
 
 # Sample Input 2 :
-# arr= [86 , 67 , 21 , 91 , -39 , 11]
+# array = [86 , 67 , 21 , 91 , -39 , 11]
 # key = 99
 
 # Output 2:
 # Key not present!
+
+# TIME COMPLEXITY :
+# Best case : O(1)
+# Average case : O(n)
+# Worst case : O(n)
+
+# SPACE COMPLEXITY : O(n)
 
