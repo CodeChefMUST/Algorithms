a8a2392a183cf21b5848677d83b83f1164fc4668
PrachiDadhich <60838285+PrachiDadhich@users.noreply.github.com>
2021-10-13 13:06:20 +0530

    Update mergesort.py

--- sorting/Merge Sort/Python/mergesort.py
+++ sorting/Merge Sort/Python/mergesort.py
@@ -1,20 +1,16 @@
-def merge(array,beg,m,end):
+def merge(array: [int], beg: int, m: int, end: int):
+    temp = [0]* (end - beg + 1)
+    i,j,k = beg, m + 1, 0
     
-
-    temp = [0]* (end-beg+1)
-    i,j,k = beg, m+1, 0
-    
-
-    while i<=m and j<=end:
-        if array[i]<=array[j]:
+    while i <= m and j <= end:
+        if array[i] <= array[j]:
             temp[k] = array[i]
-            k+=1
-            i+=1
+            k += 1
+            i += 1
         else:
             temp[k] = array[j]
-            k+=1
-            j+=1
-        
+            k += 1
+            j += 1     
 
     while i <= m:
         temp[k] = array[i]
@@ -27,11 +23,11 @@ def merge(array,beg,m,end):
         k += 1      
 
     for i in range(beg,end+1):
-        array[i] = temp[i-beg]      
+        array[i] = temp[i - beg]      
 
-def Merge_sort(array, beg, end):
-    if beg<end:
-        m = (beg+end)/2
+def Merge_sort(array: [int], beg: int, end: int):
+    if beg < end:
+        m = (beg + end)/2
         Merge_sort(array,beg,m)
         Merge_sort(array,m+1,end)
         merge(array,beg,m,end)
