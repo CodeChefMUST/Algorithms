9d27ce8eab12db59463312b4e0da1cd2ef9c0f46
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-10-17 21:32:11 +0530

    BFS in java

---
+++ Traversal Algorithms/Graph Traversals/BFS/JAVA/BFS.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class BFS{
+
+    private static int V;   // No. of vertices
+    private static LinkedList<Integer> adj[]; //Adjacency Lists
+ 
+    // Constructor
+    BFS(int v)
+    {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i){
+            adj[i] = new LinkedList();
+        }
+    }
+
+    // Function to add an edge into the graph
+    void addEdge(int v,int w)
+    {
+        adj[v].add(w);
+    }
+ 
+    // prints BFS traversal from a given source s
+    public static void BFS(int s)
+    {
+        boolean visited[] = new boolean[V]; // Mark all the vertices as not visited
+        
+        LinkedList<Integer> queue = new LinkedList<Integer>();  
+ 
+        visited[s]=true;    // Mark the current node as visited and enqueue it
+        queue.add(s);
+ 
+        while (queue.size() != 0)
+        {
+            s = queue.poll();
+            System.out.print(s+" ");    // Dequeue a vertex from queue and print it
+                       
+            Iterator<Integer> i = adj[s].listIterator();  // Get all adjacent vertices of the dequeued vertex s
+            while (i.hasNext())
+            {
+                int n = i.next();
+                if (!visited[n])
+                {
+                    visited[n] = true;  // If a adjacent has not been visited, then mark it visited and enqueue it
+                    queue.add(n);
+                }
+            }
+        }
+    }
+
+}
