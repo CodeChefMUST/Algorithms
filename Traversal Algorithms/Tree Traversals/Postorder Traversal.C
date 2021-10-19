2e11273b15240f464488846f41e783cd9bd46d62
Tanya Bansal <91904607+22tanyabansal@users.noreply.github.com>
2021-10-19 18:13:03 +0530

    Update Postorder Traversal.C

--- Traversal Algorithms/Tree Traversals/Postorder Traversal.C
+++ Traversal Algorithms/Tree Traversals/Postorder Traversal.C
@@ -1,77 +1,48 @@
-#include<stdio.h>
-#include<malloc.h>
-
-struct node {
-  int data;
-  struct node * left;
-  struct node * right;
-
+#include <stdio.h>
+struct tree {
+    int val;
+    struct tree* left;
+    struct tree* right;
 };
-struct node * tree;
-void create_tree(struct node * );
-struct node * insertElement(struct node * , int);
-void postorderTraversal(struct node * );
-int main() {
-  int option, val;
-  struct node * ptr;
-  create_tree(tree);
-  do {
-    printf("\n ****MAIN MENU***** \n");
-    printf("\n 1. Insert Element");
-    printf("\n 2. Postorder Traversal");
-    printf("\n 3. Exit");
-    printf("\n Enter your option : ");
-    scanf("%d", & option);
-    switch (option) {
-    case 1:
-      printf("\n Enter the value of the new node : ");
-      scanf("%d", & val);
-      tree = insertElement(tree, val);
-      break;
-    case 2:
-      printf("\n The elements of the tree are : \n");
-      postorderTraversal(tree);
-      break;
-    }
-  } while (option != 3);
-  return 0;
+
+typedef struct tree TreeNode;
+
+TreeNode* newTree(int data)
+{
+    TreeNode* root = (TreeNode*)malloc(sizeof(TreeNode))
+    root->val = data;
+    root->left = NULL;
+    root->right = NULL;
+    return (root);
 }
 
-void create_tree(struct node * tree) {
-  tree = NULL;
-}
-struct node * insertElement(struct node * tree, int val) {
-  struct node * ptr, * nodeptr, * parentptr;
-  ptr = (struct node * ) malloc(sizeof(struct node));
-  ptr -> data = val;
-  ptr -> left = NULL;
-  ptr -> right = NULL;
-  if (tree == NULL) {
-    tree = ptr;
-    tree -> left = NULL;
-    tree -> right = NULL;
-  } else {
-    parentptr = NULL;
-    nodeptr = tree;
-    while (nodeptr != NULL) {
-      parentptr = nodeptr;
-      if (val < nodeptr -> data)
-        nodeptr = nodeptr -> left;
-      else
-        nodeptr = nodeptr -> right;
-    }
-    if (val < parentptr -> data)
-      parentptr -> left = ptr;
-    else
-      parentptr -> right = ptr;
-  }
-  return tree;
+void postorder(TreeNode* root)
+{
+    if (root == NULL)
+        return;
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ", root->val);
 }
 
-void postorderTraversal(struct node * tree) {
-  if (tree != NULL) {
-    postorderTraversal(tree -> left);
-    postorderTraversal(tree -> right);
-    printf("%d\t", tree -> data);
-  }
+int main()
+{
+    //building the tree
+    TreeNode* t = newTree(7);
+    t->left = newTree(1);
+    t->left->left = newTree(0);
+    t->left->right = newTree(3);
+    t->left->right->left = newTree(2);
+    t->left->right->right = newTree(5);
+    t->left->right->right->left = newTree(4);
+    t->left->right->right->right = newTree(6);
+    t->right = newTree(9);
+    t->right->left = newTree(8);
+    t->right->right = newTree(10);
+
+    printf("postorder traversal of the above tree is:\n");
+    
+    postorder(t);
+
+    return 0;
 }
