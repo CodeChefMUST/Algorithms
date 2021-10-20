ab4a4782d0a47637e95bb415ccafc1a9d55ee608
SanjanaSogimatt <sanjanasogimatt@gmail.com>
2021-10-20 19:44:00 +0530

    adding bfs

---
+++ BFS.py
@@ -0,0 +1,30 @@
+import queue
+
+
+graph = {
+  '5' : ['3','7'],
+  '3' : ['2', '4'],
+  '7' : ['8'],
+  '2' : [],
+  '4' : ['8'],
+  '8' : []
+}
+visited=[]
+queue=[]
+
+def bfs(visited,graph,node):
+  
+    visited.append(node)
+    queue.append(node)
+
+    while queue:
+      m=queue.pop(0)
+      print(m , end=" ")
+
+      for i in graph[m]:
+          if i not in visited:
+              visited.append(i)
+              queue.append(i)
+print("Breadth search first : ")
+
+bfs(visited,graph,'5')
