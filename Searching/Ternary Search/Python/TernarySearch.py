2bb5b76dbf5abf68ce4bda52a7e8917909bd379c
ritika168 <91181981+ritika168@users.noreply.github.com>
2021-10-22 00:54:20 +0530

    Update TernarySearch.py

--- Searching/Ternary Search/Python/TernarySearch.py
+++ Searching/Ternary Search/Python/TernarySearch.py
@@ -13,22 +13,22 @@ def ternarySearch(ar , key , left , right):
             print( "Element found!Index:",rightmid )
             return 0;
         
-        if (ar[leftmid] == key): 
+        if ( ar[leftmid] == key ): 
 
             print( "Element found!Index:",leftmid )
             return 0;
 
-        elif (key > ar[rightmid]) : 
+        elif ( key > ar[rightmid] ) : 
             return ternarySearch( ar , key , rightmid , right)
 
-        elif (key < ar[rightmid] and key > ar[leftmid] ) : 
+        elif ( key < ar[rightmid] and key > ar[leftmid] ) : 
             return ternarySearch( ar , key , leftmid , rightmid)
 
         else: 
-            return ternarySearch(a , key , left , leftmid)
+            return ternarySearch( a , key , left , leftmid )
             
             
-    print("Key not found!")
+    print( "Key not found!" )
     
     return 0
 
